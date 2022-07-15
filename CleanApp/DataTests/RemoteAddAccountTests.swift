1d37a23cb2bd0a71872868b8cd06471e7cc8047a
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2022-07-15 02:28:27 -0300

    Test (RemoteAddAccount): Validate if the data passed is correct

--- CleanApp/DataTests/RemoteAddAccountTests.swift
+++ CleanApp/DataTests/RemoteAddAccountTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import Domain
 @testable import Data
 
 final class RemoteAddAccountTests: XCTestCase {
@@ -6,11 +7,23 @@ final class RemoteAddAccountTests: XCTestCase {
     func test_add_should_call_httpClient_with_correct_url() {
         let client = HTTPClientSpy()
         let url = URL(string: "http://any-url.com")!
-        let sut = RemoteAddAccount(url: url, client: client)
+        let sut = RemoteAddAccount(to: url, client: client)
+        let addModel = AddAccountModel.init(name: "any_name", email: "any_email@mail.com", password: "any_password", passwordConfirmation: "any_password")
         
-        sut.add()
+        sut.add(model: addModel)
         
         XCTAssertEqual(client.url, url)
+    }
+    
+    func test_add_should_call_httpClient_with_correct_data() {
+        let client = HTTPClientSpy()
+        let sut = RemoteAddAccount(to: URL(string: "http://any-url.com")!, client: client)
+        let addModel = AddAccountModel.init(name: "any_name", email: "any_email@mail.com", password: "any_password", passwordConfirmation: "any_password")
+        
+        sut.add(model: addModel)
+        
+        let data = try? JSONEncoder().encode(addModel)
+        XCTAssertEqual(client.data, data)
     }
 }
 
@@ -32,14 +45,20 @@ final class RemoteAddAccountTests: XCTestCase {
 final class HTTPClientSpy: HTTPPostClient {
     
     var url: URL?
+    var data: Data?
     
-    func post(url: URL) {
+    func post(to url: URL, with data: Data?) {
         self.url = url
+        self.data = data
     }
 }
 
+
+
+
+
 protocol HTTPPostClient {
-    func post(url: URL)
+    func post(to url: URL, with data: Data?)
 }
 
 final class RemoteAddAccount {
@@ -47,13 +66,13 @@ final class RemoteAddAccount {
     private let url: URL
     private let client: HTTPPostClient
     
-    init(url: URL, client: HTTPPostClient) {
+    init(to url: URL, client: HTTPPostClient) {
         self.url = url
         self.client = client
     }
     
-    func add() {
-        client.post(url: url)
+    func add(model: AddAccountModel) {
+        let data = try? JSONEncoder().encode(model)
+        client.post(to: url, with: data)
     }
-    
 }
