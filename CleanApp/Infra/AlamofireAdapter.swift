20246bfe902f701ba77188a608f8275f81388007
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2022-07-16 16:45:16 -0300

    Refactor: Move classes to own files

---
+++ CleanApp/Infra/AlamofireAdapter.swift
@@ -0,0 +1,71 @@
+//
+//  AlamofireAdapter.swift
+//  Infra
+//
+//  Created by Paolo Prodossimo Lopes on 16/07/22.
+//
+
+import Foundation
+import Data
+import Alamofire
+
+public final class AlamofireAdapter: HTTPPostClient {
+    public func post(to url: URL, with data: Data?, _ completion: @escaping (Result<Data?, HTTPError>) -> Void) {
+        session
+            .request(
+                url,
+                method: .post,
+                parameters: handleDataToJson(data),
+                encoding: JSONEncoding.default
+            )
+            .responseData { dataResponse in
+                switch dataResponse.result {
+                case .failure:
+                    completion(.failure(.noConnectivity))
+                case .success(let data):
+                    
+                    guard let response = dataResponse.response else {
+                        return completion(.failure(.noConnectivity))
+                    }
+                    
+                    switch response.statusCode {
+                    case 204:
+                        completion(.success(nil))
+                        
+                    case 200 ... 299:
+                        completion(.success(data))
+                        
+                    case 401:
+                        completion(.failure(.anauthorized))
+                        
+                    case 403:
+                        completion(.failure(.forbidden))
+                        
+                    case 400 ... 499:
+                        completion(.failure(.badRequest))
+                        
+                    case 500 ... 599:
+                        completion(.failure(.serverError))
+                        
+                    default:
+                        completion(.failure(.noConnectivity))
+                    }
+                }
+            }
+    }
+    
+    
+    private var session: Session
+    
+    public init(session: Session = .default) {
+        self.session = session
+    }
+    
+    private func handleDataToJson(_ data: Data?) -> [String: Any]? {
+        guard let  data = data else { return nil }
+        let jsonSerialized = try? JSONSerialization
+            .jsonObject(with: data, options: .fragmentsAllowed)
+        let json = jsonSerialized as? [String: Any]
+        return json
+    }
+}
---
+++ CleanApp/InfraTests/URLProtocolStub.swift
@@ -0,0 +1,69 @@
+import Foundation
+
+final class URLProtocolStub: URLProtocol {
+    
+    static var data: Data?
+    static var error: Error?
+    static var response: HTTPURLResponse?
+    
+    //MARK: - Observer pattern
+    
+    private static var emit: ((URLRequest) -> Void)?
+
+    static func observerRequest(_ completion: @escaping ((URLRequest) -> Void)) {
+        URLProtocolStub.emit = completion
+    }
+    
+    //MARK: - URLProtocol
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true //True indica que quer interceptar TODOS os requets
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        URLProtocolStub.emit?(request)
+        verifyClientHandlers()
+    }
+    
+    override func stopLoading() {
+        /*Non nescessary*/
+    }
+    
+    //MARK: - Methods
+    static func simulate(data: Data?, response: HTTPURLResponse?, error: Error?) {
+        URLProtocolStub.data = data
+        URLProtocolStub.response = response
+        URLProtocolStub.error = error
+    }
+    
+    //MARK: - Helpers
+    
+    private func verifyClientHandlers() {
+        configureData()
+        configureResponse()
+        configureError()
+        
+        client?.urlProtocolDidFinishLoading(self)//Notify that request was handleded and to be done
+    }
+    
+    private func configureData() {
+        if let data = URLProtocolStub.data {
+            client?.urlProtocol(self, didLoad: data)
+        }
+    }
+    
+    private func configureResponse() {
+        if let response = URLProtocolStub.response {
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+        }
+    }
+    
+    private func configureError() {
+        if let error = URLProtocolStub.error {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+    }
+}
