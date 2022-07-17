8ca427deaa6f2e79c8dbd6492fcc0b432a255ce5
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2022-07-17 08:55:42 -0300

    Test (SignUpPresenter): Ensure show error when name is not provided

--- CleanApp/PresentationTests/SignUpPresenterTests.swift
+++ CleanApp/PresentationTests/SignUpPresenterTests.swift
@@ -1,9 +1,70 @@
 import XCTest
-@testable import Presentation
 
 final class SignUpPresenterTests: XCTestCase {
     
-    func test() {
+    func test_signUp_should_show_error_message_if_name_is_not_provided() {
         
+        let model = SignUpModel(name: "", emaail: "any_email@mail.com", password: "any_password", passwordConfimation: "any_password")
+        let alertViewSpy = AlertViewSpy()
+        let sut = SignUpPresenter(alertView: alertViewSpy)
+        sut.signUp(model: model)
+        let alertModel = AlertModel(title: "Falha na validaçao", message: "O campo Nome é obrigatorio")
+        XCTAssertEqual(alertViewSpy.model, alertModel)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct AlertModel: Equatable {
+    var title: String
+    var message: String
+}
+
+protocol AlertView {
+    func showMessage(model: AlertModel)
+}
+
+struct SignUpModel {
+    var name: String?
+    var emaail: String?
+    var password: String?
+    var passwordConfimation: String?
+}
+
+final class SignUpPresenter {
+    
+    private let alertView: AlertView
+    
+    init(alertView: AlertView) {
+        self.alertView = alertView
+    }
+    
+    func signUp(model: SignUpModel) {
+        
+        if let name = model.name, name.isEmpty {
+            let alertModel = AlertModel(title: "Falha na validaçao", message: "O campo Nome é obrigatorio")
+            alertView.showMessage(model: alertModel)
+        }
+    }
+}
+
+
+
+final class AlertViewSpy: AlertView {
+    
+    var model: AlertModel?
+    
+    func showMessage(model: AlertModel) {
+        self.model = model
     }
 }
