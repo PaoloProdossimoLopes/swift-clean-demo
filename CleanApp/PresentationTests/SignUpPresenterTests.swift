0a48635c697b5c9f3ebf1f6c2d1372ad81f3082c
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2022-07-17 08:34:26 -0300

    Build (Presenter): Make framework

---
+++ CleanApp/PresentationTests/SignUpPresenterTests.swift
@@ -0,0 +1,16 @@
+//
+//  PresentationTests.swift
+//  PresentationTests
+//
+//  Created by Paolo Prodossimo Lopes on 17/07/22.
+//
+
+import XCTest
+@testable import Presentation
+
+final class SignUpPresenterTests: XCTestCase {
+    
+    func test() {
+        
+    }
+}
