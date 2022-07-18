71fc91387ada5a3e6ecd41af97a9eeff769e46ac
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2022-07-18 18:58:49 -0300

    Test (SignUpViewController): Refactor with makeSUT helper

--- CleanApp/Architecture/UI Layer/iOS/iOSUITests/SignUpViewController.swift
+++ CleanApp/Architecture/UI Layer/iOS/iOSUITests/SignUpViewController.swift
@@ -6,19 +6,26 @@ final class SignUpViewControllerTests: XCTestCase {
     
     private var sut: SignUpViewController!
     
+    override func setUp() {
+        makeSUT()
+    }
+    
     func test_loading_is_hidden_on_start() {
-        sut = .init()
-        _ = sut.view
         XCTAssertFalse(sut.loadingView.isAnimating)
     }
     
     func test_sut_implements_loadingView_protocol() {
-        sut = .init()
         XCTAssert(sut.asOpaque is LoadingView)
     }
     
     func test_sut_implements_alertView_protocol() {
-        sut = .init()
         XCTAssert(sut.asOpaque is AlertView)
+    }
+}
+
+private extension SignUpViewControllerTests {
+    func makeSUT() {
+        sut = .init()
+        _ = sut.view
     }
 }
